24163c3ac0aae34c5c814ed27bf66927b0173bd9
Lucas Muniz <lucaslima1048@gmail.com>
2023-06-09 17:45:52 -0300

    first execution of the application

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,13 @@
-console.log('ll');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => 'Server running at the http://localhost:3333');
+
