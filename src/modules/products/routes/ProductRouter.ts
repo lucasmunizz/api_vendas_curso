147a9ee3750eafc0b41eee5ed470495c5721c7bf
Lucas Muniz <lucaslima1048@gmail.com>
2023-06-11 16:26:36 -0300

    validate routes

--- src/modules/products/routes/ProductRouter.ts
+++ src/modules/products/routes/ProductRouter.ts
@@ -1,13 +1,52 @@
 import { Router } from 'express';
 import ProductController from '../controllers/ProductController';
+import { Segments, Joi, celebrate } from 'celebrate';
+import { string } from 'joi';
 
 const productRouter = Router();
 const productController = new ProductController();
 
 productRouter.get('/', productController.index);
-productRouter.get('/:id', productController.show);
-productRouter.post('/', productController.create);
-productRouter.put('/:id', productController.update);
-productRouter.delete('/:id', productController.delete);
+productRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.show,
+);
+
+productRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productController.create,
+);
+productRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
+  productController.update,
+);
+productRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productController.delete,
+);
 
 export default productRouter;
