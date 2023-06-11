753e990a7e705f4ce8075cec7fbad8eb1276b2a1
Lucas Muniz <lucaslima1048@gmail.com>
2023-06-11 00:01:05 -0300

    update oroduct service

---
+++ src/modules/products/services/UpdateProductService.ts
@@ -0,0 +1,42 @@
+import { getCustomRepository } from 'typeorm';
+import { ProductRepository } from '../typeorm/repositories/ProductRepository';
+import Product from '../typeorm/entities/Product';
+import AppError from 'src/shared/errors/AppError';
+
+interface IRequest {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export default class UpdateProductService {
+  public async execute({
+    id,
+    name,
+    price,
+    quantity,
+  }: IRequest): Promise<Product> {
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const product = await productsRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError('Product not found.');
+    }
+
+    const productExists = await productsRepository.findByName(name);
+
+    if (productExists && name !== product.name) {
+      throw new AppError('There already one product with this name');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productsRepository.save(product);
+
+    return product;
+  }
+}
