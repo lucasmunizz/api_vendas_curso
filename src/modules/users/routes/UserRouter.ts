a59de54ad52a19afc1a7c3543b0bf52fa96762bc
Lucas Muniz <lucaslima1048@gmail.com>
2023-06-15 15:50:56 -0300

    routes user avatar

--- src/modules/users/routes/UserRouter.ts
+++ src/modules/users/routes/UserRouter.ts
@@ -2,10 +2,16 @@ import { Router } from 'express';
 import UserController from '../controllers/UserController';
 import { celebrate, Joi, Segments } from 'celebrate';
 import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+import multer from 'multer';
+import UserAvatarController from '../controllers/UserAvatarController';
+import uploadConfig from '../../../config/upload';
+
+const upload = multer(uploadConfig);
 
 const routes = Router();
 
 const userController = new UserController();
+const userAvatarController = new UserAvatarController();
 
 routes.get('/', isAuthenticated, userController.index);
 routes.post(
@@ -18,6 +24,13 @@ routes.post(
     },
   }),
   userController.create,
+);
+
+routes.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
 );
 
 export default routes;
