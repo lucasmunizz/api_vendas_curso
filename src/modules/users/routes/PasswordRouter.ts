c42677bd02aab757964252385ed89ed205f82230
Lucas Muniz <lucaslima1048@gmail.com>
2023-06-19 20:39:41 -0300

    forgot password route

---
+++ src/modules/users/routes/PasswordRouter.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import SessionController from '../controllers/SessionController';
+import { Segments, celebrate, Joi } from 'celebrate';
+import ForgotPasswordController from '../controllers/ForgotPasswordController copy';
+
+const passwordRoutes = Router();
+const forgotPasswordController = new ForgotPasswordController();
+
+passwordRoutes.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
+
+export default passwordRoutes;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,12 +3,14 @@ import productsRouter from '../../../modules/products/routes/ProductRouter';
 import usersRouter from '../../../modules/users/routes/UserRouter';
 import { request } from 'http';
 import sessionRoutes from '../../../modules/users/routes/SessionRouter';
+import passwordRoutes from '../../../modules/users/routes/PasswordRouter';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionRoutes);
+routes.use('/password', passwordRoutes);
 // routes.get('/', (request: Request, response: Response) => {
 //   response.send('teste');
 // });
