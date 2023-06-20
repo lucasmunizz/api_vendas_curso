dbe7da5621e5a2499afe351d5f9b6998e68a1b2b
Lucas Muniz <lucaslima1048@gmail.com>
2023-06-20 12:39:17 -0300

    list customer service

--- src/modules/users/services/ListUserService.ts
+++ src/modules/users/services/ListUserService.ts
@@ -2,7 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 
-export default class CreateProductService {
+export default class ListUserService {
   public async execute(): Promise<User[]> {
     const usersRepository = getCustomRepository(UsersRepository);
 
